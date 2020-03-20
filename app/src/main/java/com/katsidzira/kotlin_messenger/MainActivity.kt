41122429d197a3c9349e6c8c4854fd782c6d63f1
Tafadzwa Katsidzira <tafadzwakatsidzira@Tafadzwas-MacBook-Pro-2.local>
2020-03-19 22:51:18 -0400

    added firebase auth

--- app/src/main/java/com/katsidzira/kotlin_messenger/MainActivity.kt
+++ app/src/main/java/com/katsidzira/kotlin_messenger/MainActivity.kt
@@ -3,28 +3,54 @@ package com.katsidzira.kotlin_messenger
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
 import com.katsidzira.kotlin_messenger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private val TAG: String = "main"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val name = binding.usernameEdit.text
-        val email = binding.emailEdit.text
-        val password = binding.passwordEdit.text
+        auth = FirebaseAuth.getInstance()
 
         binding.registerButton.setOnClickListener {
-            Toast.makeText(this, "Registered: $name", Toast.LENGTH_SHORT).show()
+            registerUser()
+            // move to the next activity's main fragment
         }
 
         binding.loginText.setOnClickListener {
             val intent = Intent(this, NextActivity::class.java)
             startActivity(intent)
+            // move to next activity's login fragment
         }
+    }
+
+    private fun registerUser() {
+        val name = binding.usernameEdit.text.toString()
+        val email = binding.emailEdit.text.toString()
+        val password = binding.passwordEdit.text.toString()
+
+        if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Missing or incorrect fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if (!it.isSuccessful) return@addOnCompleteListener
+                Log.d(TAG, "success: ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener {
+                Log.d(TAG, "could not create user: ${it.message}")
+                Toast.makeText(this, "Could not create user", Toast.LENGTH_SHORT).show()
+
+            }
     }
 }
